edc22a5264bed8dc7d2cb9b9f951a6ddf72ec0bb
Abdulboriy <abdulborimahamatjanov86@gmail.com>
2025-05-12 22:49:46 +0600

    dashboard added

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// import Product from "@/pages/product/Product";
+import Dashboard from "./components/dashboard/Dashboard";
+
+function App() {
+  return (
+    <>
+      <Dashboard />
+      {/* <Product /> */}
+    </>
+  );
+}
+
+export default App;
