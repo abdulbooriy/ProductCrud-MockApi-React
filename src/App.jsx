ad64af1b9317553da703bbb7bb5a9a2bc7446f9d
Abdulboriy <abdulborimahamatjanov86@gmail.com>
2025-05-13 08:17:38 +0600

    product has rendered

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-// import Product from "@/pages/product/Product";
+import Product from "@/pages/product/Product";
 import Dashboard from "./components/dashboard/Dashboard";
 
 function App() {
   return (
     <>
       <Dashboard />
-      {/* <Product /> */}
+      <Product />
     </>
   );
 }
